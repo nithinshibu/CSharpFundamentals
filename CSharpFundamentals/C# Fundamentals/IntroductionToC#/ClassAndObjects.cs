acb8a9a6ed9faeffc150cbb05b35ebb0e91bcc80
Nithin Mathew <nithin.mathew@sysfore.com>
2024-03-25 16:24:47 +0530

    Method Examples

--- CSharpFundamentals/C# Fundamentals/IntroductionToC#/ClassAndObjects.cs
+++ CSharpFundamentals/C# Fundamentals/IntroductionToC#/ClassAndObjects.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,5 +21,6 @@ namespace CSharpFundamentals.C__Fundamentals.IntroductionToC_
     public class Car
     {
         public string CarModel { get; set; } = "";
+       
     }
 }
--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,6 +1,6 @@
 ﻿#region 01_Introduction to C#
 
-using CSharpFundamentals.C__Fundamentals.IntroductionToC_;
+using CSharpFundamentals.C__Fundamentals.OOPS;
 
 //FirstCSharpApp firstCSharpApp = new FirstCSharpApp();
 //firstCSharpApp.DisplayMessage();
@@ -9,5 +9,41 @@ using CSharpFundamentals.C__Fundamentals.IntroductionToC_;
 //consoleClass.ConsoleDemo();
 
 //PrimitiveDatatypes.VariousPrimitiveDatatypes();
+
+//Product newProduct1 = new Product();
+//newProduct1.productID = 1;
+//newProduct1.productName = "Parle G";
+//newProduct1.cost = 10;
+//newProduct1.quantity = 100;
+
+//newProduct1.Method();
+
+//Products product1 = new Products();
+
+//double percentage =10;
+
+//product1.CalculateTax(in percentage);
+
+//Console.WriteLine(percentage);
+
+//product1.SetProductID(1);
+//Console.WriteLine(product1.GetProductID());
+
+//NamedParamterExample example = new NamedParamterExample();
+
+//example.ExampleMethod(data: "world", value: "hello");
+
+
+
+Student s = new Student();
+s.DisplaySubjects("Maths","Physics","Chemistry","Biology","Economics");
+
+
+//s.PrintGrade();
+
+//ref int g = ref s.DoWork();
+//g = 11;
+//s.PrintGrade();
+
 
 #endregion
