d89f0b2e28ca1d25d7908fa4d0a37e320438fd05
Nithin Mathew <nithin.mathew@sysfore.com>
2024-03-25 16:24:47 +0530

    Method Examples

---
+++ CSharpFundamentals/C# Fundamentals/OOPS/FieldsExample.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CSharpFundamentals.C__Fundamentals.OOPS;
+
+public class Product
+{
+    //fields
+    public int productID;
+    public string productName;
+    public double cost;
+    public int quantity;
+
+    public void Method()
+    {  
+        string message = displayMessage();
+
+		BankAccount account = new BankAccount("sampleConnectionString");
+        var cs = account.connectionString;
+	}
+
+    static string displayMessage()
+    {
+        
+        return "Hello World";
+    }
+}
+
+public class BankAccount
+{ 
+    public readonly string connectionString;
+
+    public BankAccount()
+    {
+        
+	}
+
+    public BankAccount(string readonlyValue)
+    {
+		this.connectionString = readonlyValue;
+	}
+}
+
+public class ConstantClass
+{
+	public const double pie = 3.1415926535897931;
+}
---
+++ CSharpFundamentals/C# Fundamentals/OOPS/MethodsExample.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals.C__Fundamentals.OOPS
+{
+	public class Products
+	{
+		//fields
+
+		public int productID;
+		public string productName;
+		public double cost;
+		public double tax;
+		public int quantityInStock;
+		public static int TotalNoOfProducts;
+		public const string CategoryName = "Electronics";
+		public readonly string dateOfPurchase;
+
+        public Products()
+        {
+            dateOfPurchase = DateTime.Now.ToShortDateString();
+        }
+
+		//Set method for productID
+
+		public void SetProductID(int value)
+		{
+			productID = value;
+		}
+
+		//Get method for productID
+
+		public int GetProductID()
+		{
+			return productID;
+		}
+
+		public static void SetTotalNoOfProducts(int total)
+		{
+			TotalNoOfProducts= total;
+		}
+		
+		public static int  GetTotalNoOfProducts()
+		{
+			return TotalNoOfProducts;
+		}
+
+		public void CalculateTax(in double percentage)
+		{			
+			tax = 5000 * percentage/100;
+		}
+		
+
+	}
+
+	public class DefaultValuesExample
+	{
+		public static void DisplayMessage(string message="Hello World")
+		{
+			Console.WriteLine(message);
+		}
+	}
+
+	public class NamedParamterExample
+	{
+		public string ExampleMethod(string value,string data)
+		{
+			return value + data;
+		}
+	}
+
+	public class ThisExample
+	{
+		public string sampleField;
+
+        public ThisExample(string sampleField)
+        {
+			this.sampleField = sampleField;
+        }
+		
+
+	}
+
+
+	public class Student
+	{
+
+		public void DisplaySubjects(params string[] subjects)
+		{
+			foreach(string sub in subjects)
+			{
+				Console.WriteLine(sub);
+			}
+		}
+
+
+		public int grade = 2;
+
+		public void PrintGrade()
+		{
+            Console.WriteLine("Grade "+ grade);
+        }
+
+		public ref int DoWork()
+		{			
+			return ref grade;
+		}
+	}
+
+	
+	
+}
